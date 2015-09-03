0e725da37da92d3a2a6c171f6d226c4b71125884
Rick Little <rick.little@simplarity.com>
2015-09-02 20:27:33 -0400

    Enabled login

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :username, presence: true, uniqueness: true
+
   has_many :tweets
 
 end
