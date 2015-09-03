7ee5061137592798ae4e9937b861d9aa1b061bec
Rick Little <rick.little@simplarity.com>
2015-09-02 20:57:13 -0400

    added epicenter ctr

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ActiveRecord::Base
 
   validates :username, presence: true, uniqueness: true
 
+  serialize :following, Array
+
   has_many :tweets
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
+  root 'epicenter#feed'
+
+  get 'epicenter_profile' => 'epicenter#show_user'
+
+  post 'now_following' => 'epicenter#now_following'
+
+  post 'unfollow' => 'epicenter#unfollow'
+
+
   resources :tweets
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'tweets#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
